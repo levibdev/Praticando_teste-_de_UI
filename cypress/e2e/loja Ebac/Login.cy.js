f3a52464c3fff69c69d5687399ab8c87cede9202
Levi Sousa Barros <levi-sousabarros@hotmail.com>
2025-07-16 11:24:12 -0300

    teste negativos e hooks

--- cypress/e2e/loja Ebac/Login.cy.js
+++ cypress/e2e/loja Ebac/Login.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress"/>
+
+beforeEach(() => {
+   cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+});
+
 describe('Funcionalidade : Login', () =>{
-      it('Deve fazer login com sucesso', () => {
-         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      it('Deve fazer login com sucesso', () => {         
          cy.get('#username').type('levi.barros@teste.com')
          cy.get('#password').type('teste123')
          cy.get('.woocommerce-form > .button').click()
@@ -9,5 +13,12 @@ describe('Funcionalidade : Login', () =>{
          cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, levi.barros (não é levi.barros? Sair)')
 
       })
+
+      it('Deve exibir uma mensagem de erro ao inserir usuario errado.', () => {
+         cy.get('#username').type('levi.barro@teste.com')
+         cy.get('#password').type('teste123')
+         cy.get('.woocommerce-form > .button').click()
+         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+      });
 
  } )
